a8cb23cf6ca9aac098cac76b83a85395508abaa1
Mohd Afzal <mohdafzal5754@gmail.com>
2025-12-06 19:05:46 +0530

    new

--- src/validators/userValidator.js
+++
@@ -1,7 +0,0 @@
-const Joi = require("joi");
-
-exports.createUserSchema = Joi.object({
-	name: Joi.string().min(1).required(),
-	email: Joi.string().email().required(),
-	age: Joi.number().integer().min(0).optional(),
-});
