6d071c51cf560eb3d1fd95ec00bd1c13eec2ac8f
Mohd Afzal <mohdafzal5754@gmail.com>
2025-12-06 22:18:03 +0530

    again com -urlencoded

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Simple in-memory "DB"
 let users = [
 	{ id: 1, name: "Alice" },
