87b63949a6b1a2f4cecd24a3ec6df4e305df0da6
Mohd Afzal <mohdafzal5754@gmail.com>
2025-12-06 18:39:13 +0530

    test basic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-const serverless = require("serverless-http");
-const app = require("./app"); // path to your express app (adjust if needed)
+// require('dotenv').config();
+// const app = require('./app');
 
-module.exports = serverless(app);
+
+// const PORT = process.env.PORT || 3000;
+
+
+// app.listen(PORT, () => {
+// console.log(`Server listening on port ${PORT} — env=${process.env.NODE_ENV || 'development'}`);
+// });
+
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.get("/home", (req, res) => {
+	res.status(200).json("Welcome, your app is working well");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running at http://localhost:${PORT}`);
+});
+
+// Export the Express API
+module.exports = app;
